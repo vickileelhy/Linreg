9aaf0c7b3f9c750155f92163d8c6e1618188ee58
Hongyan Li <hongyanli@hsph.harvard.edu>
2018-11-15 13:16:08 -0500

    test

--- R/linreg.R
+++ R/linreg.R
@@ -1,3 +1,22 @@
+#' Linear regression
+#'
+#' Runs an OLS regression not unlike \code{\link{lm}}
+#'
+#' @param x response vector (1 x n)
+#' @param y covariate matrix (p x n) with no intercept
+#'
+#' @return A list with 4 elements: coefficients, vcov, sigma, df
+#'
+#' @examples
+#' data(mtcars)
+#' X <- as.matrix(mtcars[, c("cyl", "disp", "hp")])
+#' y <- mtcars[, "mpg"]
+#' linmodEst(y, X)
+#'
+#' @export
+#'
+
+
 linmodEst <- function(x, y) {
   coef <- solve(t(x) %*% x) %*% t(x) %*% y
   ## degrees of freedom and standard deviation of residuals
